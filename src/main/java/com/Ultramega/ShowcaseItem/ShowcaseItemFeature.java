8837401ee262ed9ae6a5aac177f5d749c5da7c5f
Starforcraft <starforcraft@yahoo.com>
2023-04-19 19:56:53 +0200

    Fixed trying to showcase causing you to log out sometimes

--- src/main/java/com/Ultramega/ShowcaseItem/ShowcaseItemFeature.java
+++ src/main/java/com/Ultramega/ShowcaseItem/ShowcaseItemFeature.java
@@ -17,7 +17,6 @@ import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.Style;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.FormattedCharSequence;
-import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Blocks;
@@ -33,6 +32,8 @@ import java.util.List;
 @Mod.EventBusSubscriber(modid = ShowcaseItem.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ShowcaseItemFeature {
 	public static float alphaValue = 1F;
+
+	private static long lastShadeTimestamp = -1;
 
 	@OnlyIn(Dist.CLIENT)
 	public static void renderItemForMessage(PoseStack poseStack, FormattedCharSequence sequence, float x, float y, int color) {
@@ -58,74 +59,53 @@ public class ShowcaseItemFeature {
 	@OnlyIn(Dist.CLIENT)
 	public static void keyboardEvent(ScreenEvent.KeyPressed.Pre event) {
 		Minecraft mc = Minecraft.getInstance();
-		Screen screen = event.getScreen();
-		if(InputConstants.isKeyDown(mc.getWindow().getWindow(), ModKeyBindings.SHOWCASE_ITEM.getKey().getValue()) && keyModifierPressed(mc) && screen instanceof AbstractContainerScreen<?> gui) {
+		if(InputConstants.isKeyDown(mc.getWindow().getWindow(), ModKeyBindings.SHOWCASE_ITEM.getKey().getValue()) && keyModifierPressed(mc)) {
+			keyPressed();
+		}
+	}
+
+	public static void keyPressed() {
+		Minecraft mc = Minecraft.getInstance();
+		Screen screen = mc.screen;
+
+		if(screen instanceof AbstractContainerScreen<?> gui) {
 			List<? extends GuiEventListener> children = gui.children();
 			for(GuiEventListener c : children)
-				if(c instanceof EditBox tf)
+				if(c instanceof EditBox tf) {
 					if(tf.isFocused())
 						return;
+				}
 
 			Slot slot = gui.getSlotUnderMouse();
 			if(slot != null) {
 				ItemStack stack = slot.getItem();
 
 				if(!stack.isEmpty()) {
-					ShareItemMessage message = new ShareItemMessage(slot.getSlotIndex());
+					if(mc.level != null && mc.level.getGameTime() - lastShadeTimestamp > 10) {
+						lastShadeTimestamp = mc.level.getGameTime();
+					} else return;
+
+					ShareItemMessage message = new ShareItemMessage(slot.index, gui.getMenu().containerId);
 					ShowcaseItemNetwork.sendToServer(message);
 				}
 			}
 		}
 	}
 
-	public static void shareItem(ServerPlayer player, int slot) {
-		Inventory inv = player.getInventory();
-		if(slot >= 0 && slot < inv.getContainerSize()) {
-			ItemStack stack = inv.getItem(slot);
-			if(!stack.isEmpty()) {
-				MutableComponent comp = Component.translatable("showcaseitem.misc.shared_item", player.getName());
-				Component itemComp = stack.getDisplayName();
+	public static void shareItem(ServerPlayer player, int slot, int containedId) {
+		if(player.containerMenu.containerId == containedId) {
+			var slots = player.containerMenu.slots;
+			if (slot >= 0 && slots.size() > slot) {
+				ItemStack stack = slots.get(slot).getItem();
+				if (!stack.isEmpty()) {
+					MutableComponent comp = Component.translatable("showcaseitem.misc.shared_item", player.getName());
+					Component itemComp = stack.getDisplayName();
 
-				comp.append(itemComp);
-				player.server.getPlayerList().getPlayers().forEach(p -> p.sendSystemMessage(comp));
+					comp.append(itemComp);
+					player.server.getPlayerList().getPlayers().forEach(p -> p.sendSystemMessage(comp));
+				}
 			}
 		}
-	}
-
-	private static boolean keyModifierPressed(Minecraft mc) {
-		int keyModifierInt = checkLeftKeyModifier();
-		int keyModifierInt2 = checkRightKeyModifier();
-		if(keyModifierInt != 0)
-			return InputConstants.isKeyDown(mc.getWindow().getWindow(), keyModifierInt);
-		else if(keyModifierInt2 != 0)
-			return InputConstants.isKeyDown(mc.getWindow().getWindow(), keyModifierInt2);
-		else
-			return false;
-	}
-
-	private static int checkLeftKeyModifier() {
-		KeyModifier keyModifier = ModKeyBindings.SHOWCASE_ITEM.getKeyModifier();
-		int keyModifierInt = 0;
-		if(keyModifier.equals(KeyModifier.CONTROL))
-			keyModifierInt = 341;
-		else if(keyModifier.equals(KeyModifier.ALT))
-			keyModifierInt = 342;
-		else if(keyModifier.equals(KeyModifier.SHIFT))
-			keyModifierInt = 340;
-		return keyModifierInt;
-	}
-
-	private static int checkRightKeyModifier() {
-		KeyModifier keyModifier = ModKeyBindings.SHOWCASE_ITEM.getKeyModifier();
-		int keyModifierInt = 0;
-		if(keyModifier.equals(KeyModifier.CONTROL)) {
-			keyModifierInt = 345;
-		} else if(keyModifier.equals(KeyModifier.ALT)) {
-			keyModifierInt = 346;
-		} else if(keyModifier.equals(KeyModifier.SHIFT)) {
-			keyModifierInt = 344;
-		}
-		return keyModifierInt;
 	}
 
 	public static MutableComponent createStackComponent(ItemStack stack, MutableComponent component) {
@@ -156,7 +136,7 @@ public class ShowcaseItemFeature {
 			ItemStack stack = contents != null ? contents.getItemStack() : ItemStack.EMPTY;
 
 			if (stack.isEmpty())
-				stack = new ItemStack(Blocks.BARRIER);
+				stack = new ItemStack(Blocks.BARRIER); // for invalid icon
 
 			int shift = mc.font.width(before);
 
@@ -166,7 +146,9 @@ public class ShowcaseItemFeature {
 				PoseStack poseStack = RenderSystem.getModelViewStack();
 
 				poseStack.pushPose();
+
 				poseStack.mulPoseMatrix(pose.last().pose());
+
 				poseStack.translate(shift + x, y, 0);
 				poseStack.scale(0.5f, 0.5f, 0.5f);
 				mc.getItemRenderer().renderGuiItem(stack, 0, 0);
@@ -177,5 +159,41 @@ public class ShowcaseItemFeature {
 				alphaValue = 1F;
 			}
 		}
+	}
+
+	private static boolean keyModifierPressed(Minecraft mc) {
+		int keyModifierInt = checkLeftKeyModifier();
+		int keyModifierInt2 = checkRightKeyModifier();
+		if(keyModifierInt != -1)
+			return InputConstants.isKeyDown(mc.getWindow().getWindow(), keyModifierInt);
+		else if(keyModifierInt2 != -1)
+			return InputConstants.isKeyDown(mc.getWindow().getWindow(), keyModifierInt2);
+		else
+			return true;
+	}
+
+	private static int checkLeftKeyModifier() {
+		KeyModifier keyModifier = ModKeyBindings.SHOWCASE_ITEM.getKeyModifier();
+		int keyModifierInt = -1;
+		if(keyModifier.equals(KeyModifier.CONTROL))
+			keyModifierInt = 341;
+		else if(keyModifier.equals(KeyModifier.ALT))
+			keyModifierInt = 342;
+		else if(keyModifier.equals(KeyModifier.SHIFT))
+			keyModifierInt = 340;
+		return keyModifierInt;
+	}
+
+	private static int checkRightKeyModifier() {
+		KeyModifier keyModifier = ModKeyBindings.SHOWCASE_ITEM.getKeyModifier();
+		int keyModifierInt = -1;
+		if(keyModifier.equals(KeyModifier.CONTROL)) {
+			keyModifierInt = 345;
+		} else if(keyModifier.equals(KeyModifier.ALT)) {
+			keyModifierInt = 346;
+		} else if(keyModifier.equals(KeyModifier.SHIFT)) {
+			keyModifierInt = 344;
+		}
+		return keyModifierInt;
 	}
 }
--- src/main/java/com/Ultramega/ShowcaseItem/message/ShareItemMessage.java
+++ src/main/java/com/Ultramega/ShowcaseItem/message/ShareItemMessage.java
@@ -8,18 +8,23 @@ import java.io.Serial;
 
 public class ShareItemMessage implements IMessage {
     @Serial
-    private static final long serialVersionUID = 2204175080232208579L;
+    private static final long serialVersionUID = 2204175080232208578L;
 
+    public int containerId;
     public int slot;
 
-    public ShareItemMessage(int slot) {
+    public ShareItemMessage() { }
+
+    public ShareItemMessage(int slot, int containerId) {
         this.slot = slot;
+        this.containerId = containerId;
     }
 
+    @Override
     public boolean receive(Context context) {
         ServerPlayer player = context.getSender();
         if (player != null && player.server != null)
-            context.enqueueWork(() -> ShowcaseItemFeature.shareItem(player, slot));
+            context.enqueueWork(() -> ShowcaseItemFeature.shareItem(player, slot, containerId));
 
         return true;
     }
