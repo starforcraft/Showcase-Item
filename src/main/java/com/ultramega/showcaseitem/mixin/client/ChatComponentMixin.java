083c2ed9d58dfa7e29223abc1e9e98828e4efbf7
Starforcraft <starforcraft@yahoo.com>
2024-11-09 18:57:41 +0100

    Added chat heads compat

--- src/main/java/com/ultramega/showcaseitem/mixin/client/ChatComponentMixin.java
+++ src/main/java/com/ultramega/showcaseitem/mixin/client/ChatComponentMixin.java
@@ -5,16 +5,18 @@ import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.ChatComponent;
 import net.minecraft.util.FormattedCharSequence;
+import net.neoforged.fml.ModList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ChatComponent.class)
-public class ChatComponentMixin {
+@Mixin(value = ChatComponent.class)
+public abstract class ChatComponentMixin {
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Lnet/minecraft/util/FormattedCharSequence;III)I"))
-    private int drawItems(GuiGraphics guiGraphics, Font font, FormattedCharSequence formattedCharSequence, int x, int y, int color) {
-        ShowcaseItemFeature.renderItemForMessage(guiGraphics, formattedCharSequence, x, y, color);
+    private int showcaseItem$drawItems(GuiGraphics graphics, Font font, FormattedCharSequence formattedCharSequence, int x, int y, int color) {
+        int xOffset = ModList.get().isLoaded("chat_heads") ? 10 : 0;
+        ShowcaseItemFeature.renderItemForMessage(graphics, formattedCharSequence, x + xOffset, y, color);
 
-        return guiGraphics.drawString(font, formattedCharSequence, x, y, color);
+        return graphics.drawString(font, formattedCharSequence, x, y, color);
     }
 }
--- src/main/java/com/ultramega/showcaseitem/mixin/client/ItemBlockRenderTypesMixin.java
+++ src/main/java/com/ultramega/showcaseitem/mixin/client/ItemBlockRenderTypesMixin.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import static net.minecraft.client.renderer.ItemBlockRenderTypes.getChunkRenderType;
 
 @Mixin(ItemBlockRenderTypes.class)
-public class ItemBlockRenderTypesMixin {
+public abstract class ItemBlockRenderTypesMixin {
     @Inject(method = "getRenderType(Lnet/minecraft/world/level/block/state/BlockState;)Lnet/minecraft/client/renderer/RenderType;", at = @At("HEAD"), cancellable = true)
     private static void overrideRenderType(BlockState state, CallbackInfoReturnable<RenderType> cir) {
         if (ShowcaseItemFeature.alphaValue != 1.0F) {
