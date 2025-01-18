26e9b94c4986d0ff4c529473781af14d0a0c5b86
Starforcraft <starforcraft@yahoo.com>
2025-01-18 13:27:38 +0100

    Fixed OpenGL Error when key binding is unbound

--- src/main/java/com/ultramega/showcaseitem/mixin/client/ChatComponentMixin.java
+++ src/main/java/com/ultramega/showcaseitem/mixin/client/ChatComponentMixin.java
@@ -5,17 +5,16 @@ import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.ChatComponent;
 import net.minecraft.util.FormattedCharSequence;
-import net.neoforged.fml.ModList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = ChatComponent.class)
 public abstract class ChatComponentMixin {
+    // TODO: add chat heads mod compat
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Lnet/minecraft/util/FormattedCharSequence;III)I"))
     private int showcaseItem$drawItems(GuiGraphics graphics, Font font, FormattedCharSequence formattedCharSequence, int x, int y, int color) {
-        int xOffset = ModList.get().isLoaded("chat_heads") ? 10 : 0;
-        ShowcaseItemFeature.renderItemForMessage(graphics, formattedCharSequence, x + xOffset, y, color);
+        ShowcaseItemFeature.renderItemForMessage(graphics, formattedCharSequence, x, y, color);
 
         return graphics.drawString(font, formattedCharSequence, x, y, color);
     }
